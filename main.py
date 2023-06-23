df6e0fd46af7f2175fb1a1995651ccafef9f16eb
utpal-maersk <73114132+utpal-maersk@users.noreply.github.com>
2023-06-23 11:22:34 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+def get_driver():
+  options=webdriver.ChromeOptions()
+  options.add_argument("disable-infobars")
+  options.add_argument("start-maximized")
+  options.add_argument("disable-dev-shm-usage")
+  options.add_argument("no-sandbox")
+  options.add_experimental_option("excludeSwitches",["enable-automation"])
+  options.add_argument("disable-blink-features=AutomationControlled")
+  driver=webdriver.Chrome(options=options)
+  driver.get("http://automated.pythonanywhere.com")
+  return driver
+
+def main():
+  driver=get_driver()
+  element=driver.find_element(By.XPATH,"/html/body/div[1]/div/h1[1]")
+  return element.text
+
+print(main())
